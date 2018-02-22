ee01a3e9f119c5e26ec98c4026be3df1dab4934d
Shebin John <30735581+remedcu@users.noreply.github.com>
2018-02-22 17:28:53 +0530

    Created a basic index.php

---
+++ index.php
@@ -0,0 +1,202 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="dns-prefetch" href="https://assets-cdn.github.com">
+        <link rel="dns-prefetch" href="https://avatars0.githubusercontent.com">
+        <link rel="dns-prefetch" href="https://avatars1.githubusercontent.com">
+        <link rel="dns-prefetch" href="https://avatars2.githubusercontent.com">
+        <link rel="dns-prefetch" href="https://avatars3.githubusercontent.com">
+        <link rel="dns-prefetch" href="https://github-cloud.s3.amazonaws.com">
+        <link rel="dns-prefetch" href="https://user-images.githubusercontent.com/">
+
+        <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/frameworks-7a12427f1445.css" integrity="sha512-ehJCfxRFqU8Lt/+Hwc4cvp41sPisCp1U2d03SlbM3aKVZcxiYDTVmBsAtOVgAtZ912JFOebwrPXr/JcDNd6bGA==" media="all" rel="stylesheet">
+        <link crossorigin="anonymous" href="https://assets-cdn.github.com/assets/github-fca0cb55603c.css" integrity="sha512-/KDLVWA8Zk1fk+dIfhP6IAZzt71eiA0TYWwuXaN7rA9z8xkfYFxRJgmgqD3EG1vRF6efmrpg9kr3HMoJH6+qGw==" media="all" rel="stylesheet">
+
+        <meta name="viewport" content="width=device-width">
+  
+        <title>Dynamite Arts Studio</title>
+        <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="GitHub">
+        <link rel="fluid-icon" href="https://github.com/phpbro/dynamitearts.com/raw/master/Favicon.png" title="GitHub">
+  
+        <meta content="https://avatars3.githubusercontent.com/u/29937098?s=400&amp;v=4" property="og:image">
+
+        <link rel="assets" href="https://assets-cdn.github.com/">
+        <link rel="web-socket" href="wss://live.github.com/_sockets/VjI6MjUxNjg2MDg2OjM4OWUwMWVhNTJmMmVkMDZhOWM5YWU2NWMyMmJkOWQ1YzA0MTFkZmNmYWY3YTY5ZTQ1ZDI1OWExM2M4Njk2NTk=--db94e6b8c50c6b1e9ec4cd00095ba5d947636834">
+        <meta name="pjax-timeout" content="1000">
+        <link rel="sudo-modal" href="/sessions/sudo_modal">
+
+        <meta class="js-ga-set" name="dimension1" content="Logged In">
+
+        <meta name="hostname" content="github.com">
+        <meta name="user-login" content="phpbro">
+
+        <meta name="expected-hostname" content="github.com">
+        <meta name="js-proxy-site-detection-payload" content="NzczZDVhNjA4Mjg3Yjk3ODliZGVmOWM5MDBkNjQ3OWNiNjA2OTNhOGU0MzZmYzFhOThmOWNlYmRkNDA4ZTM5MHx7InJlbW90ZV9hZGRyZXNzIjoiMTM3Ljk3LjEyMC41MiIsInJlcXVlc3RfaWQiOiJFQ0MyOjJGQzA6MTQ0Nzc4NDoyMzc3NDkyOjVBOEU4RTVFIiwidGltZXN0YW1wIjoxNTE5MjkxOTk5LCJob3N0IjoiZ2l0aHViLmNvbSJ9">
+
+        <meta name="enabled-features" content="UNIVERSE_BANNER,FREE_TRIALS,MARKETPLACE_HERO_CARD_UPLOADER,MARKETPLACE_INSIGHTS,MARKETPLACE_INSIGHTS_CONVERSION_PERCENTAGES">
+
+        <meta name="html-safe-nonce" content="ca19df3cccadfc93f893c1d8e23b4e74d522a268">
+
+        <meta http-equiv="x-pjax-version" content="e86940185a18758ba7e3cb5638026ae7">
+  
+        <meta name="go-import" content="github.com/phpbro/dynamitearts.com git https://github.com/phpbro/dynamitearts.com.git">
+
+        <meta name="browser-stats-url" content="https://api.github.com/_private/browser/stats">
+
+        <meta name="browser-errors-url" content="https://api.github.com/_private/browser/errors">
+
+        <link rel="mask-icon" href="https://assets-cdn.github.com/pinned-octocat.svg" color="#000000">
+        <link rel="icon" type="image/x-icon" class="js-site-favicon" href="https://github.com/phpbro/dynamitearts.com/raw/master/Favicon.png">
+
+        <meta name="theme-color" content="#1e2327">
+
+        <meta name="u2f-support" content="true">
+
+        <link rel="manifest" href="/manifest.json" crossorigin="use-credentials">
+
+        <meta name="selected-link" value="repo_source" data-pjax-transient="true">
+        <meta content="/<user-name>/<repo-name>" data-pjax-transient="true" name="analytics-location">
+        <meta name="request-id" content="F679:A1AA:12459BF:1F8AB69:5A8EA23F" data-pjax-transient="true">
+        <link rel="canonical" href="https://github.com/phpbro/dynamitearts.com" data-pjax-transient="true">
+        <link href="https://github.com/phpbro/dynamitearts.com/commits/master.atom" rel="alternate" title="Recent Commits to dynamitearts.com:master" type="application/atom+xml" data-pjax-transient="true">
+    </head>
+    
+    <body class="logged-in env-production page-blob intent-mouse">
+
+        <div role="main" class="application-main ">
+        <div itemscope="" itemtype="http://schema.org/SoftwareSourceCode" class="">
+        <div id="js-repo-pjax-container" data-pjax-container=""></div>
+        <div class="container new-discussion-timeline experiment-repo-nav  ">
+        <div class="repository-content ">
+        <div id="readme" class="readme boxed-group clearfix announce instapaper_body md">
+            <article class="markdown-body entry-content" itemprop="text">
+                <h1>
+                    <a href="#dynamite-arts-studio" aria-hidden="true" class="anchor" id="user-content-dynamite-arts-studio">
+                        <svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16">
+                            <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
+                        </svg>
+                    </a>
+                    Dynamite Arts Studio
+                </h1>
+                <p>Contact me at <a href="mailto:admin@dynamitearts.com">admin@dynamitearts.com</a></p>
+                <p>Or in social networks:</p>
+                <p>Facebook: <a href="https://www.facebook.com/niyesh.niyeshshaji" rel="nofollow">https://www.facebook.com/niyesh.niyeshshaji</a></p>
+                <p>Behance: <a href="https://www.behance.net/niyeshshaj40c5" rel="nofollow">https://www.behance.net/niyeshshaj40c5</a></p>
+                <p>Linkedin Personal: <a href="https://www.linkedin.com/in/niyesh/" rel="nofollow">https://www.linkedin.com/in/niyesh/</a></p>
+                <p>Instagram: <a href="https://www.instagram.com/dynamite_artz_studio/" rel="nofollow">https://www.instagram.com/dynamite_artz_studio/</a></p>
+                <p>Linkedin Company: <a href="https://www.linkedin.com/company/dynamitearts/" rel="nofollow">https://www.linkedin.com/company/dynamitearts/</a></p>
+                <p>The below are some of my latest works</p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/App%20UI%20Design.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/App%20UI%20Design.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Apple.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Apple.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Clash%20Of%20Clan%20COC.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Clash%20Of%20Clan%20COC.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Clash%20of%20Clan%20Sculpt.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Clash%20of%20Clan%20Sculpt.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Drone.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Drone.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Dynamite%20Arts%20Logo.png" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Dynamite%20Arts%20Logo.png" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Dynamite%20Arts%20with%20Logo.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Dynamite%20Arts%20with%20Logo.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Fantasy%20Ball.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Fantasy%20Ball.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Fantasy%20Fish.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Fantasy%20Fish.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/First%20Character%20Sculpt.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/First%20Character%20Sculpt.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Food%20App%20Splash%20Screen.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Food%20App%20Splash%20Screen.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Food%20App%20UI.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Food%20App%20UI.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Food%20Spot%20Logo.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Food%20Spot%20Logo.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Love%20Token%20with%20Logo.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Love%20Token%20with%20Logo.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Love%20Token%20with%20small%20Logo.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Love%20Token%20with%20small%20Logo.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Texploxe%20Logo.png" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Texploxe%20Logo.png" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Texploxe%20with%20Logo.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Texploxe%20with%20Logo.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/UI%20Design%201.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/UI%20Design%201.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/UI%20Design%202.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/UI%20Design%202.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/UI%20Presentation.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/UI%20Presentation.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>
+                    <a href="https://github.com/phpbro/dynamitearts.com/blob/master/Website%20UI.jpg" target="_blank">
+                        <img src="https://github.com/phpbro/dynamitearts.com/raw/master/Website%20UI.jpg" alt="" style="max-width:100%;">
+                    </a>
+                </p>
+                <p>Soon will add more!</p>
+            </article>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </body>
+</html>
